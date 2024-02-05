eddfacbc4c23226ca7a1bdd1fef8b0d5454a527b
Akshatha Nayak <abn5461@submit03.2e.hpc.psu.edu>
2024-02-05 10:00:06 -0500

    align and allocate multimapped reads with allo

--- scripts/3_align.sh
+++ scripts/3_align.sh
@@ -5,29 +5,52 @@
 #SBATCH --ntasks=4
 #SBATCH --mem=80GB
 #SBATCH --partition=sla-prio
-#SBATCH --job-name=bowtie
-#SBATCH --output=/storage/group/sam77/default/lab/akshatha/predict_tf_binding/logs/bowtie.out
+#SBATCH --job-name=align
+#SBATCH --output=/storage/group/sam77/default/lab/akshatha/predict_tf_binding/logs/align.out
 umask 007
 
 # Activate conda environment
 source /storage/work/abn5461/miniforge3/bin/activate /storage/work/abn5461/miniforge3/envs/allo
 
-OUT_DIR=/storage/group/sam77/default/lab/akshatha/predict_tf_binding/data/oct4_alignments
-mkdir -p $OUT_DIR
 cd /storage/group/sam77/default/lab/akshatha/predict_tf_binding/data
 
-# Align with bowtie
+# Sequences
 SEQS=(ATAC_EB_rep1 ATAC_EB_rep2 ATAC_EB+12h-iNIL_rep1 ATAC_EB+12h-iNIL_rep2)
+
+# Align with bowtie
+OUT_DIR=/storage/group/sam77/default/lab/akshatha/predict_tf_binding/data/oct4_alignments
+mkdir -p $OUT_DIR
 for SEQ in ${SEQS[@]}
 do
     bowtie -q ~/group/genomes/mm10/mm10 -1 oct4_trimmed/${SEQ}/${SEQ}_R1_trimmed_paired.fastq.gz -2 oct4_trimmed/${SEQ}/${SEQ}_R2_trimmed_paired.fastq.gz -S ${OUT_DIR}/${SEQ}_MMR.sam --best --strata -m 100 -k 100 -p 8 --chunkmbs 200
 done
+bowtie -q ~/group/genomes/mm10/mm10 control.fastq.gz -S ${OUT_DIR}/control_MMR.sam --best --strata -m 100 -k 100 -p 8 --chunkmbs 200
 
-# Convert sam to bam
+# Sort files
 cd $OUT_DIR
-ls *sam | xargs -I {} -n 1 samtools view -h -Sb {} -o {}.bam
+mkdir -p sorted
+for SEQ in ${SEQS[@]}
+do
+    samtools collate -o sorted/${SEQ}_MMR_sort.sam ${SEQ}_MMR.sam
+done
+samtools collate -o sorted/control_MMR_sort.sam control_MMR.sam
+
+# Allocate multi-mapped reads
+cd ${OUT_DIR}/sorted
+for SEQ in ${SEQS[@]}
+do
+    echo "allo ${SEQ}_MMR_sort.sam -seq se -p 8"
+    allo ${SEQ}_MMR_sort.sam -seq se -p 8
+done
+allo control_MMR_sort.sam -seq se -p 8 --random
 
 # Organize files
-mkdir -p sam bam
-mv *sam sam/
+cd $OUT_DIR
+mkdir -p ${OUT_DIR}/allo
+mv sorted/*allo* allo/
+
+# Convert sam to bam
+cd ${OUT_DIR}/allo
+ls *sam | xargs -I {} -n 1 samtools view -h -Sb {} -o {}.bam
+mkdir -p bam
 mv *bam bam/
